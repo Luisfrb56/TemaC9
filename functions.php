fd3ae35ffd95036456db09523f471e9b1ba59a0e
Luisfrb56 <luisfrb56@gmail.com>
2018-11-26 09:55:25 +0000

    Widget y menu

--- functions.php
+++ functions.php
@@ -1,19 +1,23 @@
-0
-/**
- * Register our sidebars and widgetized areas.
- *
- */
-function luis_widgets_init() {
+<?php
+function twentyseventeen_child_widgets_init() {
 
 	register_sidebar( array(
-		'name'          => 'Prueba',
-		'id'            => 'Luisfr',
-		'before_widget' => '<div>',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h2 class="rounded">',
+		'name'          => __( 'funcionalfin', 'twentyseventeen' ),
+		'id'            => 'funcion',
+	    'description'   => __( 'Agregamos widget', 'twentyseventeen-child' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title MiClase">',
 		'after_title'   => '</h2>',
 	) );
 
 }
-add_action( 'widgets_init', 'luis_widgets_init' );
+add_action( 'widgets_init', 'twentyseventeen_child_widgets_init' );
+
+function register_my_menu() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'register_my_menu' );
+
+
 ?>
